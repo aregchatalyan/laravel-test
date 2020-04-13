bb24b13cd0a03e00addc39855b088464f2d2280b
Areg <aregooogle@gmail.com>
2020-04-13 23:00:14 +0400

    Creating migartions

---
+++ app/Employee.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Employee extends Model
+{
+    protected $table = 'employees';
+
+    public function company()
+    {
+        return $this->belongsTo('companies', 'id');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+Route::resource('/employee', 'EmployeeController');
+Route::resource('/company', 'CompanyController');
